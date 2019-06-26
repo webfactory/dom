fed2cc0f020a7f112b07b6dbb4a847d3a1a333b7
Matthias Pigulla <mp@webfactory.de>
2019-06-26 07:27:17 +0000

    Use local catalog also for HTML5 (polyglot) parsing

--- src/Webfactory/Dom/HTMLParsingHelper.php
+++ src/Webfactory/Dom/HTMLParsingHelper.php
@@ -16,6 +16,29 @@ abstract class HTMLParsingHelper extends BaseParsingHelper {
         'hx'   => 'http://purl.org/NET/hinclude' // fuer HInclude http://mnot.github.io/hinclude/; ein Weg um z.B. Controller in Symfony per Ajax zu embedden
     );
 
+    public function __construct()
+    {
+        libxml_set_external_entity_loader(function ($public, $system, $context) {
+            if (isset($public)) {
+                $catalogDir = __DIR__ . '/../../../xml-catalog/';
+                switch ($public) {
+                    case '-//W3C//DTD XHTML 1.0 Strict//EN':
+                        return $catalogDir . 'xhtml1-strict.dtd';
+                    case '-//W3C//ENTITIES Latin 1 for XHTML//EN':
+                        return $catalogDir . 'xhtml-lat1.ent';
+                    case '-//W3C//ENTITIES Symbols for XHTML//EN':
+                        return $catalogDir . 'xhtml-symbol.ent';
+                    case '-//W3C//ENTITIES Special for XHTML//EN':
+                        return $catalogDir . 'xhtml-special.ent';
+                    case '-//W3C//DTD XHTML 1.0 Transitional//EN':
+                        return $catalogDir . 'xhtml1-transitional.dtd';
+                }
+            }
+
+            return $system;
+        });
+    }
+
     protected function wrapFragment($fragment, $declaredNamespaces)
     {
         return "<html {$this->xmlNamespaceDeclaration($declaredNamespaces)}>$fragment</html>";
