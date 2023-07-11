1a4e5c4f4732a41778e71ef6581e5124ae8d0fce
Martin Güthler <mg@webfactory.de>
2023-07-11 16:00:41 +0200

    Ändere Reihenfolge der XML-Namespaces im HTMLParsingHelper

--- src/Webfactory/Dom/HTMLParsingHelper.php
+++ src/Webfactory/Dom/HTMLParsingHelper.php
@@ -11,8 +11,8 @@ namespace Webfactory\Dom;
 abstract class HTMLParsingHelper extends BaseParsingHelper {
 
     protected $implicitNamespaces = array(
-        ''     => 'http://www.w3.org/1999/xhtml', // default ns
         'html' => 'http://www.w3.org/1999/xhtml', // für XPath
+        ''     => 'http://www.w3.org/1999/xhtml', // default ns
         'hx'   => 'http://purl.org/NET/hinclude' // fuer HInclude http://mnot.github.io/hinclude/; ein Weg um z.B. Controller in Symfony per Ajax zu embedden
     );
 
